61b3283507cec51c6db72ac5ea3513f5d0c78308
s2hmuel <s2hmuel@gmail.com>
2023-11-08 00:24:44 +0900

    8단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -37,6 +37,8 @@ public class App {
                 case "삭제":
                     delete(rq);
                     break;
+                case "수정":
+                    edit(rq);
             }
         }
     }
@@ -95,5 +97,36 @@ public class App {
         }
 
         return -1;
+    }
+
+    private void edit(Rq rq){
+        int quoteID = rq.getParamAsInt("id", 0);
+
+        if (quoteID == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return; // 함수를 끝낸다.
+        }
+
+        int index = findQuoteIndexById(quoteID);
+
+        if (index == -1) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", quoteID);
+            return;
+        }
+
+        Quotation quotation = quotations.get(index);
+
+        System.out.printf("명언(기존) : %s\n", quotation.getContent());
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.printf("작가(기존) : %s\n", quotation.getWriter());
+        System.out.print("작가 : ");
+        String writer = scanner.nextLine();
+
+        quotation.setContent(content);
+        quotation.setWriter(writer);
+
+        System.out.printf("%d번 명언을 수정되었습니다.\n", quoteID);
     }
 }
