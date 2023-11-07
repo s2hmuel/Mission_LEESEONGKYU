c0cc2cac6583227b30827044cdb26e9935b7c289
s2hmuel <s2hmuel@gmail.com>
2023-11-07 20:45:40 +0900

    첫번째 리팩토링

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,24 +3,31 @@ package com.ll;
 import java.util.Scanner;
 
 public class App {
-    public void run() {
-        System.out.println("== 명언앱 ==");
+
         Scanner scanner = new Scanner(System.in);
         int quoteID = 0;
+    public void run() {
+        System.out.println("== 명언앱 ==");
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-                System.out.print("작가 : ");
-                String writer = scanner.nextLine();
-                quoteID++;
-                System.out.printf("%d번 명언이 등록되었습니다.\n", quoteID);
+
+            switch (cmd){
+                case "종료":
+                    return;
+                case "등록":
+                    write();
             }
         }
+    }
+
+    void write(){
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String writer = scanner.nextLine();
+        quoteID++;
+        System.out.printf("%d번 명언이 등록되었습니다.\n", quoteID);
     }
 }
