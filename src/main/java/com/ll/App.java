41f07ee37d51e87c252ab727b200a52a1d033e5e
s2hmuel <s2hmuel@gmail.com>
2023-11-07 20:39:51 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class App {
     public void run() {
         System.out.println("== 명언앱 ==");
-        System.out.print("명령) ");
         Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+
 
         while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String writer = scanner.nextLine();
             }
         }
     }
