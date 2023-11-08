3f77357d34def410fd3df36d6abe02350ee65436
s2hmuel <s2hmuel@gmail.com>
2023-11-08 16:38:01 +0900

    9단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,13 @@
 package com.ll;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class App {
 
@@ -14,11 +19,12 @@ public class App {
         scanner = new Scanner(System.in);
         quoteID = 0;
         quotations = new ArrayList<>();
-
     }
 
     public void run() {
+
         System.out.println("== 명언앱 ==");
+        readTextFile();
 
         while (true) {
             System.out.print("명령) ");
@@ -27,6 +33,7 @@ public class App {
 
             switch (rq.getAction()){
                 case "종료":
+                    writeTextFile();
                     return;
                 case "등록":
                     write();
@@ -58,7 +65,6 @@ public class App {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-
         if(quotations.isEmpty()){
             System.out.println("등록된 명언이 없습니다!");
         }
@@ -75,15 +81,14 @@ public class App {
         if (QuoteID == 0) {
             System.out.println("id를 정확히 입력해주세요.");
         }
-        int index = findQuoteIndexById(QuoteID);
 
+        int index = findQuoteIndexById(QuoteID);
         if (index == -1) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", QuoteID);
             return;
         }
 
         quotations.remove(index);
-
         System.out.printf("%d번 명언을 삭제되었습니다.\n", QuoteID);
     }
 
@@ -95,7 +100,6 @@ public class App {
                 return i;
             }
         }
-
         return -1;
     }
 
@@ -108,25 +112,66 @@ public class App {
         }
 
         int index = findQuoteIndexById(quoteID);
-
         if (index == -1) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", quoteID);
             return;
         }
 
         Quotation quotation = quotations.get(index);
-
         System.out.printf("명언(기존) : %s\n", quotation.getContent());
         System.out.print("명언 : ");
         String content = scanner.nextLine();
-
         System.out.printf("작가(기존) : %s\n", quotation.getWriter());
         System.out.print("작가 : ");
         String writer = scanner.nextLine();
-
         quotation.setContent(content);
         quotation.setWriter(writer);
-
         System.out.printf("%d번 명언을 수정되었습니다.\n", quoteID);
+    }
+
+    private void readTextFile() {
+
+        String filePath = "/Users/s2hmuel/Documents/quotation.txt";
+        int linesToSkip = 2;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int lineCount = 0;
+            while ((line = reader.readLine()) != null) {
+                if (lineCount < linesToSkip) {
+                    lineCount++;
+                    continue;
+                }
+                String[] quoteBits = line.split("/");
+                if(quoteBits.length == 3){
+                    String stirngQuoteID = quoteBits[0].trim();
+                    int quoteID = Integer.parseInt(stirngQuoteID);
+                    String writer = quoteBits[1];
+                    String content = quoteBits[2];
+                    fileToQuotation(quoteID, writer, content);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void writeTextFile() {
+        String filePath = "/Users/s2hmuel/Documents/quotation.txt";
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
+             PrintWriter writer = new PrintWriter(bufferedWriter)) {
+                writer.println("번호 / 작가 / 명언");
+                writer.println("----------------------");
+            for (int i = 0; i < quotations.size(); i++){
+                Quotation quotation = quotations.get(i);
+                writer.printf("%d / %s / %s\n", quotation.getQuoteID(), quotation.getWriter(), quotation.getContent());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fileToQuotation(int quoteID, String writer, String content){
+        Quotation quotation = new Quotation(quoteID, writer, content);
+        quotations.add(quotation);
     }
 }
